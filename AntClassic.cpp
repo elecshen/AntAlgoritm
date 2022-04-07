ccae7695aec8c3cb07f3fa6706b72e424c3a150e
elserverhtml <53811726+elserverhtml@users.noreply.github.com>
2022-04-08 01:51:36 +0600

    Разделение на два файла

--- AntClassic.cpp
+++ AntClassic.cpp
@@ -1,1 +1,165 @@
 #include "AntClassic.h"
+
+void AntClassic::getReady() {
+	for (int i = 0; i < N; i++) {
+		swarmOfAnts[i].orderOfVisitedCities = new int[N] {-1};
+		for (int y = 0; y < N; y++) {
+			wayArr[i][y].proximity = pow(1.0 / wayArr[i][y].length, b);
+		}
+	}
+	srand(time(0));
+}
+
+void AntClassic::howManyWaysIsAvailable() {
+	for (int i = 0; i < N; i++)
+		if (orderOfVisitedCities[i] == -1) {
+			numOfAvailableWays = N - i - 1;
+			break;
+		}
+}
+
+void AntClassic::calculateProbability() {
+	double sum = 0;
+	for (int i = 0; i < N; i++) {
+		if (!checkedCity[i])
+			probabilities[i] = pow(wayArr[currentCity][i].pheromones, a) * wayArr[currentCity][i].proximity;
+		else
+			probabilities[i] = 0;
+
+		sum += probabilities[i];
+	}
+	for (int i = 0; i < N; i++)
+		probabilities[i] = probabilities[i] / sum;
+}
+
+int AntClassic::chooseWay() {
+	double choiñe = double(rand()) / RAND_MAX, tempChoice = 0;
+	int num = 0, realNum = 0;
+	while (num < N) {
+		tempChoice += probabilities[num];
+		if (probabilities[num++] != 0) realNum = num;
+		if (tempChoice > choiñe) break;
+	}
+	return realNum - 1;
+}
+
+void AntClassic::goToCity(int cityNumber) {
+	checkedCity[cityNumber] = true;
+	for (int i = 0; i < N; i++)
+		if (orderOfVisitedCities[i] == -1) {
+			orderOfVisitedCities[i] = cityNumber;
+			break;
+		}
+	currentCity = cityNumber;
+}
+
+void AntClassic::saveResultOfWay(int numOfAnt) {
+	for (int i = 0; i < N - 1; i++) {
+		swarmOfAnts[numOfAnt].lengthOfWay += wayArr[orderOfVisitedCities[i]][orderOfVisitedCities[i + 1]].length;
+		swarmOfAnts[numOfAnt].orderOfVisitedCities[i] = orderOfVisitedCities[i];
+	}
+	swarmOfAnts[numOfAnt].lengthOfWay += wayArr[orderOfVisitedCities[N - 1]][orderOfVisitedCities[0]].length;
+	swarmOfAnts[numOfAnt].orderOfVisitedCities[N - 1] = orderOfVisitedCities[N - 1];
+}
+
+void AntClassic::clearWay() {
+	for (int i = 0; i < N; i++) {
+		checkedCity[i] = 0;
+		orderOfVisitedCities[i] = -1;
+	}
+}
+
+void AntClassic::vaporizePheromones() {
+	for (int i = 0; i < N; i++) {
+		for (int y = 0; y < N; y++) {
+			wayArr[i][y].pheromones *= P;
+		}
+	}
+}
+
+void AntClassic::spreadPheromones() {
+	double newPheramones;
+	for (int i = 0; i < N; i++) {
+		for (int y = 0; y < N - 1; y++) {
+			newPheramones = Q / swarmOfAnts[i].lengthOfWay;
+			wayArr[swarmOfAnts[i].orderOfVisitedCities[y]][swarmOfAnts[i].orderOfVisitedCities[y + 1]].pheromones += newPheramones;
+			wayArr[swarmOfAnts[i].orderOfVisitedCities[y + 1]][swarmOfAnts[i].orderOfVisitedCities[y]].pheromones += newPheramones;
+		}
+		newPheramones = Q / swarmOfAnts[i].lengthOfWay;
+		wayArr[swarmOfAnts[i].orderOfVisitedCities[N - 1]][swarmOfAnts[i].orderOfVisitedCities[0]].pheromones += newPheramones;
+		wayArr[swarmOfAnts[i].orderOfVisitedCities[0]][swarmOfAnts[i].orderOfVisitedCities[N - 1]].pheromones += newPheramones;
+	}
+}
+
+void AntClassic::saveMinWayAndRestartSwarm() {
+	int minIndex = 0;
+	double min = swarmOfAnts[minIndex].lengthOfWay;
+	for (int i = 1; i < N; i++)
+		if (swarmOfAnts[i].lengthOfWay < min) {
+			min = swarmOfAnts[i].lengthOfWay;
+			minIndex = i;
+		}
+
+	minWayLength = min;
+	for (int i = 0; i < N; i++)
+		orderOfCitiesWithMinWayLength[i] = swarmOfAnts[minIndex].orderOfVisitedCities[i];
+
+	for (int i = 0; i < N; i++) {
+		swarmOfAnts[i].lengthOfWay = 0;
+		for (int y = 0; y < N; y++)
+			swarmOfAnts[i].orderOfVisitedCities[y] = -1;
+	}
+}
+
+AntClassic::AntClassic(int NumOfCity) : N(NumOfCity) {
+	wayArr = new Way * [N];
+	for (int i = 0; i < N; i++) {
+		wayArr[i] = new Way[N];
+	}
+	checkedCity = new bool[N] {0};
+	orderOfVisitedCities = new int[N] {-1};
+	orderOfCitiesWithMinWayLength = new int[N] {-1};
+	probabilities = new double[N];
+	lengthsOfWay = new double[N] {0};
+	swarmOfAnts = new Ant[N];
+}
+
+void AntClassic::initWays(double** lengths, double** pheromones) {
+	for (int i = 0; i < N; i++)
+		for (int y = 0; y < N; y++) {
+			wayArr[i][y].length = lengths[i][y];
+			wayArr[i][y].pheromones = pheromones[i][y];
+		}
+}
+
+void AntClassic::initConsts(double const_a, double const_b, double const_Q, double const_P) {
+	a = const_a;
+	b = const_b;
+	Q = const_Q;
+	P = const_P;
+}
+
+void AntClassic::start(int limitOfSwarmIterations) {
+	getReady();
+	while (limitOfSwarmIterations-- > 0) {
+		for (int i = 0; i < N; i++) {
+			goToCity(i);
+			for (int y = 0; y < N - 1; y++) {
+				calculateProbability();
+				goToCity(chooseWay());
+			}
+			saveResultOfWay(i);
+			clearWay();
+		}
+		vaporizePheromones();
+		spreadPheromones();
+		saveMinWayAndRestartSwarm();
+	}
+}
+
+void AntClassic::printMinWay() {
+	std::cout << "Length of shortest way is " << minWayLength << std::endl << "Order of cities:";
+	for (int i = 0; i < N; i++) {
+		std::cout << " " << orderOfCitiesWithMinWayLength[i + 1];
+	}
+}
--- AntClassic.h
+++ AntClassic.h
@@ -26,169 +26,35 @@ private:
 	double* probabilities, * lengthsOfWay;
 	Ant* swarmOfAnts;
 
-	void getReady() {
-		for (int i = 0; i < N; i++) {
-			swarmOfAnts[i].orderOfVisitedCities = new int[N] {-1};
-			for (int y = 0; y < N; y++) {
-				wayArr[i][y].proximity = pow(1.0 / wayArr[i][y].length, b);
-			}
-		}
-		srand(time(0));
-	}
+	void getReady();
 
-	void howManyWaysIsAvailable() {
-		for (int i = 0; i < N; i++)
-			if (orderOfVisitedCities[i] == -1) {
-				numOfAvailableWays = N - i - 1;
-				break;
-			}
-	}
+	void howManyWaysIsAvailable();
 
-	void calculateProbability() {
-		double sum = 0;
-		for (int i = 0; i < N; i++) {
-			if (!checkedCity[i])
-				probabilities[i] = pow(wayArr[currentCity][i].pheromones, a) * wayArr[currentCity][i].proximity;
-			else
-				probabilities[i] = 0;
+	void calculateProbability();
 
-			sum += probabilities[i];
-		}
-		for (int i = 0; i < N; i++)
-			probabilities[i] = probabilities[i] / sum;
-	}
+	int chooseWay();
 
-	int chooseWay() {
-		double choiñe = double(rand()) / RAND_MAX, tempChoice = 0;
-		int num = 0, realNum = 0;
-		while (num < N) {
-			tempChoice += probabilities[num];
-			if (probabilities[num++] != 0) realNum = num;
-			if (tempChoice > choiñe) break;
-		}
-		return realNum - 1;
-	}
+	void goToCity(int cityNumber);
 
-	void goToCity(int cityNumber) {
-		checkedCity[cityNumber] = true;
-		for (int i = 0; i < N; i++)
-			if (orderOfVisitedCities[i] == -1) {
-				orderOfVisitedCities[i] = cityNumber;
-				break;
-			}
-		currentCity = cityNumber;
-	}
+	void saveResultOfWay(int numOfAnt);
 
-	void saveResultOfWay(int numOfAnt) {
-		for (int i = 0; i < N - 1; i++) {
-			swarmOfAnts[numOfAnt].lengthOfWay += wayArr[orderOfVisitedCities[i]][orderOfVisitedCities[i + 1]].length;
-			swarmOfAnts[numOfAnt].orderOfVisitedCities[i] = orderOfVisitedCities[i];
-		}
-		swarmOfAnts[numOfAnt].lengthOfWay += wayArr[orderOfVisitedCities[N - 1]][orderOfVisitedCities[0]].length;
-		swarmOfAnts[numOfAnt].orderOfVisitedCities[N - 1] = orderOfVisitedCities[N - 1];
-	}
+	void clearWay();
 
-	void clearWay() {
-		for (int i = 0; i < N; i++) {
-			checkedCity[i] = 0;
-			orderOfVisitedCities[i] = -1;
-		}
-	}
+	void vaporizePheromones();
 
-	void vaporizePheromones() {
-		for (int i = 0; i < N; i++) {
-			for (int y = 0; y < N; y++) {
-				wayArr[i][y].pheromones *= P;
-			}
-		}
-	}
+	void spreadPheromones();
 
-	void spreadPheromones() {
-		double newPheramones;
-		for (int i = 0; i < N; i++) {
-			for (int y = 0; y < N - 1; y++) {
-				newPheramones = Q / swarmOfAnts[i].lengthOfWay;
-				wayArr[swarmOfAnts[i].orderOfVisitedCities[y]][swarmOfAnts[i].orderOfVisitedCities[y + 1]].pheromones += newPheramones;
-				wayArr[swarmOfAnts[i].orderOfVisitedCities[y + 1]][swarmOfAnts[i].orderOfVisitedCities[y]].pheromones += newPheramones;
-			}
-			newPheramones = Q / swarmOfAnts[i].lengthOfWay;
-			wayArr[swarmOfAnts[i].orderOfVisitedCities[N - 1]][swarmOfAnts[i].orderOfVisitedCities[0]].pheromones += newPheramones;
-			wayArr[swarmOfAnts[i].orderOfVisitedCities[0]][swarmOfAnts[i].orderOfVisitedCities[N - 1]].pheromones += newPheramones;
-		}
-	}
-
-	void saveMinWayAndRestartSwarm() {
-		int minIndex = 0;
-		double min = swarmOfAnts[minIndex].lengthOfWay;
-		for (int i = 1; i < N; i++)
-			if (swarmOfAnts[i].lengthOfWay < min) {
-				min = swarmOfAnts[i].lengthOfWay;
-				minIndex = i;
-			}
-
-		minWayLength = min;
-		for (int i = 0; i < N; i++)
-			orderOfCitiesWithMinWayLength[i] = swarmOfAnts[minIndex].orderOfVisitedCities[i];
-
-		for (int i = 0; i < N; i++) {
-			swarmOfAnts[i].lengthOfWay = 0;
-			for (int y = 0; y < N; y++)
-				swarmOfAnts[i].orderOfVisitedCities[y] = -1;
-		}
-	}
+	void saveMinWayAndRestartSwarm();
 
 public:
-	AntClassic(int NumOfCity) : N(NumOfCity) {
-		wayArr = new Way * [N];
-		for (int i = 0; i < N; i++) {
-			wayArr[i] = new Way[N];
-		}
-		checkedCity = new bool[N] {0};
-		orderOfVisitedCities = new int[N] {-1};
-		orderOfCitiesWithMinWayLength = new int[N] {-1};
-		probabilities = new double[N];
-		lengthsOfWay = new double[N] {0};
-		swarmOfAnts = new Ant[N];
-	}
+	AntClassic(int NumOfCity);
 
-	void initWays(double** lengths, double** pheromones) {
-		for (int i = 0; i < N; i++)
-			for (int y = 0; y < N; y++) {
-				wayArr[i][y].length = lengths[i][y];
-				wayArr[i][y].pheromones = pheromones[i][y];
-			}
-	}
+	void initWays(double** lengths, double** pheromones);
 
-	void initConsts(double const_a, double const_b, double const_Q, double const_P) {
-		a = const_a;
-		b = const_b;
-		Q = const_Q;
-		P = const_P;
-	}
+	void initConsts(double const_a, double const_b, double const_Q, double const_P);
 
-	void start(int limitOfSwarmIterations) {
-		getReady();
-		while (limitOfSwarmIterations-- > 0) {
-			for (int i = 0; i < N; i++) {
-				goToCity(i);
-				for (int y = 0; y < N - 1; y++) {
-					calculateProbability();
-					goToCity(chooseWay());
-				}
-				saveResultOfWay(i);
-				clearWay();
-			}
-			vaporizePheromones();
-			spreadPheromones();
-			saveMinWayAndRestartSwarm();
-		}
-	}
+	void start(int limitOfSwarmIterations);
 
-	void printMinWay() {
-		std::cout << "Length of shortest way is " << minWayLength << std::endl << "Order of cities:";
-		for (int i = 0; i < N; i++) {
-			std::cout << " " << orderOfCitiesWithMinWayLength[i + 1];
-		}
-	}
+	void printMinWay();
 };
 
